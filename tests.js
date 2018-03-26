4f4df32bff300905b085032865cb03cea85829db
DonBattery <mikloslorinczi.it@gmail.com>
2018-03-26 15:38:32 +0200

    minor fix

--- tests.js
+++ tests.js
@@ -5,10 +5,10 @@ const getEnv = require('./tools/envservice').getEnv;
 const test = require('tape');
 
 test('Envservice test', function (t) {
-  t.same({}, getEnv({}), 'Testing with an empty connObj');
+  t.same({}, getEnv({}), 'getEnv with an empty connObj');
 
   t.same({ host: 'a', port: 1111, user: 'b', password: 'c', database: 'd' },
-  getEnv({host: 'a', port: 1111, user: 'b', password: 'c', database: 'd'}), 'Testing with a custom connObj');
+  getEnv({host: 'a', port: 1111, user: 'b', password: 'c', database: 'd'}), 'getEnv with a custom connObj');
 
   t.same({
     host: process.env.RDS_HOSTNAME,
@@ -16,7 +16,7 @@ test('Envservice test', function (t) {
     user: process.env.RDS_USER,
     password: process.env.RDS_PASSWORD,
     database: process.env.RDS_DATABASE
-    }, getEnv(), 'Testing with actual Environment Variables');
-    
+    }, getEnv(), 'getEnv with actual Environment Variables');
+
   t.end();
 });
